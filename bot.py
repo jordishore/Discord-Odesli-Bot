c7dd5b2be7724bd14045fb6388dad396b5f514b7
jordishore <93233324+jordishore@users.noreply.github.com>
2023-06-21 23:48:59 +1000

    v1.1.1 Added logging.

--- bot.py
+++ bot.py
@@ -1,9 +1,20 @@
+import logging
 import os
+import sys
+import time
 
 import discord
 import requests
 from discord.ext import commands
 from dotenv import load_dotenv
+
+logger = logging.getLogger('discord-odesli-bot')
+logger.setLevel(logging.INFO)
+logger.handler = logging.StreamHandler(sys.stdout)
+logger.handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+logger.handler.setFormatter(formatter)
+logger.addHandler(logger.handler)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,24 +27,29 @@ bot = commands.Bot(command_prefix='?', intents=intents)
 
 def get_link(url):
     api_link = "https://api.song.link/v1-alpha.1/links?url=" + url + "&userCountry=AU" #update country code to suit
+    logger.info(f'Getting Songlink')
     request = requests.get(api_link)
     data = request.json()
     songlink = str(data['pageUrl'])
-    print(songlink)
+    logger.info(f'Songlink: {songlink}')
     return songlink
 
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    logger.info(f'{bot.user.name} has connected to Discord!')
 
 
 @bot.command(name='sl')
 async def sl(ctx, songlink: str):
-    print(f'{ctx.author}: {ctx.message.content}')
+    logger.info(f'{ctx.author.display_name} shared: {ctx.message.content}')
     response = get_link(songlink)
-    await discord.Message.delete(ctx.message, delay=None)
     await ctx.send(f"{ctx.author.mention} shared a song: {response}")
+    logger.info('Deleting original message...')
+    await discord.Message.delete(ctx.message, delay=10)
+    time.sleep(10)
+    logger.info('Message deleted.')
+    
 
 
 bot.run(TOKEN)
